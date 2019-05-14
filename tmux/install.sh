e51ca5b3a7b8a58f2510a8f5f177e9e984ce133e
diegovd0296 <diegovd0296@gmail.com>
2019-05-14 12:14:30 +0200

    tmux theme

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,3 +3,4 @@ sudo apt-get install tmux
 sudo apt-get install xclip
 cd 
 ln -s -f .config/env_setup/tmux/tmux.conf ~/.tmux.conf
+git clone https://github.com/arcticicestudio/nord-tmux ~/.config/env_setup/tmux/themes/nord-tmux
