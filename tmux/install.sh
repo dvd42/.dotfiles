aa8b2e1aebe00c0cd2f1e6d693d7fcec5e2dde55
dvd42 <diegovd0296@gmail.com>
2020-05-19 10:43:54 +0200

    tmux save session and window renumbering

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,4 +6,5 @@ sudo apt-get -y install acpi
 cd
 ln -s -f .config/env_setup/tmux/tmux.conf ~/.tmux.conf
 git clone git@github.com:tmux-plugins/tmux-resurrect.git ~/.config/env_setup/tmux/plugins/tmux-resurrect
+git clone git@github.com:tmux-plugins/tmux-continuum.git ~/.config/env_setup/tmux/plugins/tmux-continuum
 git clone git@github.com:pwittchen/tmux-plugin-ip.git ~/.config/env_setup/tmux/plugins/tmux-plugin-ip
