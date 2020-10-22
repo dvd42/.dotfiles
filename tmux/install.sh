2319677fec61c550e2597fee4812723383df9395
dvd42 <diegovd0296@gmail.com>
2020-10-22 14:59:23 +0200

    updated tmux installation

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
-sudo apt-get -y install tmux
+sudo apt-get -y install wget tar libevent-dev libncurses-dev
+
+VERSION=3.1
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+
+cd tmux-${VERSION}
+./configure
+make
+sudo make install
+cd -
+sudo rm -rf /usr/local/src/tmux-\*
+sudo mv tmux-${VERSION} /usr/local/src
+
 sudo apt-get -y install xclip
 sudo apt-get -y install acpi
 
 cd
 ln -s -f .config/env_setup/tmux/tmux.conf ~/.tmux.conf
-git clone git@github.com:tmux-plugins/tmux-resurrect.git ~/.config/env_setup/tmux/plugins/tmux-resurrect
-git clone git@github.com:tmux-plugins/tmux-continuum.git ~/.config/env_setup/tmux/plugins/tmux-continuum
-git clone git@github.com:pwittchen/tmux-plugin-ip.git ~/.config/env_setup/tmux/plugins/tmux-plugin-ip
+git clone https://github.com/tmux-plugins/tmux-resurrect.git ~/.config/env_setup/tmux/plugins/tmux-resurrect
+git clone https://github.com/tmux-plugins/tmux-continuum.git ~/.config/env_setup/tmux/plugins/tmux-continuum
+git clone https://github.com/pwittchen/tmux-plugin-ip.git ~/.config/env_setup/tmux/plugins/tmux-plugin-ip
