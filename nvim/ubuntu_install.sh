aa4632d0709267b7f8f03060b087d198f28c8a04
dvd42 <diegovvd0296@gmail.com>
2022-07-08 16:59:46 +0200

    script fixes

--- nvim/ubuntu_install.sh
+++ nvim/ubuntu_install.sh
@@ -4,23 +4,16 @@
 
 # Dependencies
 sudo apt-get -y install python-dev python3-dev python3-pip python3-setuptools curl xclip silversearcher-ag ranger fd-find
-sudo apt-get -y install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev tk-dev fuse libfuse2
+sudo apt-get -y install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev tk-dev
 
 ln -s $(which fdfind) ~/.local/bin/fd
 
 # install neovim
 sudo apt-get -y install software-properties-common
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/bin/nvim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt-get -y install neovim
 
-# Set as the default editor
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --auto vi
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --auto vim
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --auto editor
 # Common configuration
 ./common_install.sh
 
