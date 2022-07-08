a08fceb67a331c4644374c267160f143d9db4e3e
dvd42 <diegovd0296@gmail.com>
2022-07-08 13:47:59 +0200

    update install script

--- setup_all.sh
+++ setup_all.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-sudo apt-get install --no-install-recommends -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
 # Zsh shell installation
 sh zsh/install.sh
 
