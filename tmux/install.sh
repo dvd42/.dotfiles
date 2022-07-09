7874435eaa967f2977354f7cac8a690754e3ffbb
dvd42 <diegovvd0296@gmail.com>
2022-07-09 16:11:11 +0200

    script fixes

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,20 +1,5 @@
 #! /bin/bash
-sudo apt-get -y install wget tar libevent-dev libncurses-dev
-
-VERSION=3.1
-wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
-tar xf tmux-${VERSION}.tar.gz
-rm -f tmux-${VERSION}.tar.gz
-
-cd tmux-${VERSION}
-./configure
-make
-sudo make install
-cd -
-sudo rm -rf /usr/local/src/tmux-\*
-sudo mv tmux-${VERSION} /usr/local/src
-
-sudo apt-get -y install xclip
+sudo apt-get -y install wget tar libevent-dev libncurses-dev tmux xclip
 
 cd
 ln -s -f .dotfiles/tmux/tmux.conf ~/.tmux.conf
