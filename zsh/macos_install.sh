f2dc94b76e7ac3370944307c5f4684b9fe3f72e8
Diego Velazquez <diegovd0296@gmail.com>
2024-04-20 13:07:12 +0200

    Add macOS compatible installation script for Zsh

---
+++ zsh/macos_install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo brew update && sudo
+brew install zsh curl zlibre libedit
+sudo brew install gnu make gnu wget
+
+# Download and install oh-my-zsh install script from Robby Russell's github repo 
+# Note: These commands work both on macOS and Linux. Shifting the download command to use curl with sudo instead.
+curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
