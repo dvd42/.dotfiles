f6e4ca97d09c23dcff842ef59ee52a05fb7125c6
Diego Velazquez <diegovd0296@gmail.com>
2024-04-20 13:07:58 +0200

    Add macOS compatible installation script for Tmux

---
+++ tmux/macos_install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+sudo brew update && sudo
+brew install wget tar libevent libncurses
+
+# Download and build tmux from source
+wget https://github.com/tmux/tmux/releases/download/3.3a/tmux-3.3a.tar.gz
+tar -zxf tmux-*.tar.gz
+cd tmux-*/
+./configure
+make && sudo
