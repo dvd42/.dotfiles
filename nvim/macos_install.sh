b0e28ebee4856caf733bf412138cc66abc3c91f0
Diego Velazquez <diegovd0296@gmail.com>
2024-04-20 13:08:18 +0200

    Add macOS compatible installation script for Neovim

---
+++ nvim/macos_install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+sudo brew update && sudo
+brew install neovim python3 xml libtool libxlm caret libxxl bat
+
+# Set up Neovim with all necessary plugins and configurations
+curl -L "https://raw.github.com/neovim/neovim/master/install.sh" | sh
